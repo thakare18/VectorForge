9a225ef87d9be2c10c6a973c223443e9897ceb09
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-07-25 18:25:20 +0530

    add API routes for modular route handling

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,15 @@
-
 const express = require("express");
 
 const { healthCheck } = require("../controllers/health.controller");
 
+
+const apiRoutes = require("./api.routes");
+
 const router = express.Router();
 
 router.get("/", healthCheck);
+
+
+router.use("/api", apiRoutes);
 
 module.exports = router;
